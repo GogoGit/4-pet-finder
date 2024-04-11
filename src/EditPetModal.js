c5b181d9c6e9bde1354ce538323980f0feee8878
GogoGit <6920876+GogoGit@users.noreply.github.com>
2024-04-11 19:33:37 -0400

    Consolidated both the New and Edit PetModal.js files.

--- src/EditPetModal.js
+++ src/EditPetModal.js
@@ -1,88 +1,11 @@
-import { useState, useRef } from "react";
 import Modal from "react-modal";
+import PetForm from "./PetForm";
 
 const EditPetModal = ({ pet, onCancel, onSave }) => {
-  const [name, setName] = useState(pet.name);
-  const [kind, setKind] = useState(pet.kind);
-  const [photo, setPhoto] = useState(pet.photo);
-
-  const [errors, setErrors] = useState(null);
-  const [saving, setSaving] = useState(false);
-  const photoInput = useRef();
-
-  const submit = (event) => {
-    event.preventDefault();
-    setSaving(true);
-    onSave({
-      ...pet, //This is how we are getting the pet id back in???
-      /* comment from Dani W   
-        "Kevin I think what's happening is the spread operator is getting all the key val pairs from the pet object, 
-        but then we overwrite the name, kind and photo keys.
-        You can try it in the console. If you use the spread after the new fields btw you'll
-        end up with the original pet object { name: newname, kind: kind, ...pet }" */
-
-      name,
-      kind,
-      photo,
-    }).catch((error) => {
-      console.log(error);
-      setErrors(error);
-      setSaving(false);
-    });
-  };
-
-  const updatePhoto = () => {
-    const file = photoInput.current.files && photoInput.current.files[0];
-
-    if (file) {
-      const reader = new FileReader();
-      reader.onloadend = () => setPhoto(reader.result);
-      reader.readAsDataURL(file);
-    }
-  };
-
   return (
     <Modal isOpen={true} onRequestClose={onCancel}>
       <h2>Edit Pet</h2>
-      <form className="pet-form" onSubmit={submit}>
-        {photo && <img alt="the pet" src={photo} />}
-
-        <label htmlFor="photo">Photo</label>
-        <input type="file" id="photo" ref={photoInput} onChange={updatePhoto} />
-
-        <label htmlFor="name">Name</label>
-        <input
-          //   require  //this wouild be front end validation
-          type="text"
-          id="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        {/* This is back end error checking since this comes from the server */}
-        {errors && errors.name && <div className="error">{errors.name}</div>}
-
-        <label htmlFor="kind">Kind</label>
-        <select
-          name="kind"
-          id="kind"
-          value={kind}
-          onChange={(e) => setKind(e.target.value)}
-        >
-          <option value="">Choose a kind</option>
-          <option value="cat">Cat</option>
-          <option value="dog">Dog</option>
-        </select>
-
-        {/* This is back end error checking since this comes from the server */}
-        {errors && errors.kind && <div className="error">{errors.kind}</div>}
-
-        <button disabled={saving} type="button" onClick={onCancel}>
-          Cancel
-        </button>
-        <button type="submit" disabled={saving}>
-          Save
-        </button>
-      </form>
+      <PetForm pet={pet} onCancel={onCancel} onSave={onSave} />
     </Modal>
   );
 };
--- src/NewPetModal.js
+++ src/NewPetModal.js
@@ -1,82 +1,12 @@
-import { useState, useRef } from "react";
+import React from "react";
 import Modal from "react-modal";
+import PetForm from "./PetForm";
 
-// const NewPetModal = ({ isOpen, onCancel, onSave }) => {
 const NewPetModal = ({ onCancel, onSave }) => {
-  const [name, setName] = useState("");
-  const [kind, setKind] = useState("");
-  const [photo, setPhoto] = useState(null);
-
-  const [errors, setErrors] = useState(null);
-  const [saving, setSaving] = useState(false);
-  const photoInput = useRef();
-
-  const submit = (event) => {
-    event.preventDefault();
-    setSaving(true);
-    onSave({
-      name,
-      kind,
-      photo,
-    }).catch((error) => {
-      console.log(error);
-      setErrors(error);
-      setSaving(false);
-    });
-  };
-
-  const updatePhoto = () => {
-    const file = photoInput.current.files && photoInput.current.files[0];
-
-    if (file) {
-      const reader = new FileReader();
-      reader.onloadend = () => setPhoto(reader.result);
-      reader.readAsDataURL(file);
-    }
-  };
-
   return (
     <Modal isOpen={true} onRequestClose={onCancel}>
       <h2>New Pet</h2>
-      <form className="pet-form" onSubmit={submit}>
-        {photo && <img alt="the pet" src={photo} />}
-
-        <label htmlFor="photo">Photo</label>
-        <input type="file" id="photo" ref={photoInput} onChange={updatePhoto} />
-
-        <label htmlFor="name">Name</label>
-        <input
-          //   require  //this wouild be front end validation
-          type="text"
-          id="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        {/* This is back end error checking since this comes from the server */}
-        {errors && errors.name && <div className="error">{errors.name}</div>}
-
-        <label htmlFor="kind">Kind</label>
-        <select
-          name="kind"
-          id="kind"
-          value={kind}
-          onChange={(e) => setKind(e.target.value)}
-        >
-          <option value="">Choose a kind</option>
-          <option value="cat">Cat</option>
-          <option value="dog">Dog</option>
-        </select>
-
-        {/* This is back end error checking since this comes from the server */}
-        {errors && errors.kind && <div className="error">{errors.kind}</div>}
-
-        <button disabled={saving} type="button" onClick={onCancel}>
-          Cancel
-        </button>
-        <button type="submit" disabled={saving}>
-          Save
-        </button>
-      </form>
+      <PetForm onCancel={onCancel} onSave={onSave} />
     </Modal>
   );
 };
